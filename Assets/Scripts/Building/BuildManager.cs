3db155b2abddf05eecc45dd59319570ade754b14
Scobmyster <jaa.scobbie@gmail.com>
2020-10-31 09:56:18 +0000

    Initial commit
    Can raycast to building plots and select and deselect them at will.

---
+++ Assets/Scripts/Building/BuildManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BuildManager : MonoBehaviour
+{
+
+    public GameObject chosenTowerGOToBuild;
+    public BuildPlot plotChosenToBuildOn;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            int layerMask = 1 << 8;
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.yellow);
+                if(hit.collider.gameObject.GetComponent<BuildPlot>())
+                {
+                    Debug.Log("Hit plot");
+                    if (plotChosenToBuildOn != null)
+                        plotChosenToBuildOn.ClickedOff();
+                    plotChosenToBuildOn = hit.collider.gameObject.GetComponent<BuildPlot>();
+                    plotChosenToBuildOn.ClickedOn();
+                }
+               
+            }
+            else if (plotChosenToBuildOn != null)
+            {
+                plotChosenToBuildOn.ClickedOff();
+            }
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Building/BuildPlot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildPlot : MonoBehaviour
+{
+
+    public Material materialToSwitchToWhenSelected;
+    public Material defaultMaterial;
+    public GameObject buildPosition;
+    private GameObject builtGO;
+
+    public void ClickedOn()
+    {
+        GetComponent<MeshRenderer>().material = materialToSwitchToWhenSelected;
+    }
+
+    public void ClickedOff()
+    {
+        GetComponent<MeshRenderer>().material = defaultMaterial;
+    }
+
+    public void BuildOn(GameObject towerGO)
+    {
+        builtGO = (GameObject)Instantiate(towerGO, buildPosition.transform.position, Quaternion.identity, this.transform);
+    }
+
+}
