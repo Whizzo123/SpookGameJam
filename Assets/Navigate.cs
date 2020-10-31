30e86c841efa18eef7be166a00a420f6538c44b6
Scobmyster <jaa.scobbie@gmail.com>
2020-10-31 07:42:59 +0000

    Implemented Basic Navigation
    Added so we can navigate to a chosen position of a gameobject as long as it is on the navmesh

---
+++ Assets/Navigate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.XR.WSA.Input;
+
+public class Navigate : MonoBehaviour
+{
+    public GameObject targetPos;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(targetPos.transform.position);
+    }
+}
