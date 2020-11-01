c6d21b04de1c8ec41a5c452492db83328480d495
TaniCorn <63819551+TaniCorn@users.noreply.github.com>
2020-11-01 17:39:59 +0000

    Bug fixes
    Plyaer healkth increase, and level screen fix

---
+++ Assets/Scripts/NewLevelSelect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class NewLevelSelect : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("FirstLevel");
+    }
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("SecondLevel");
+    }
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("ThirdLevel");
+    }
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene("FourthLevel");
+    }
+    public void LoadLevel5()
+    {
+        SceneManager.LoadScene("FifthLevel");
+    }
+}
