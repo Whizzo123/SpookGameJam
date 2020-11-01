954a72d8c420ae3e654ebd8dd81f9cb54fc5993f
SilverMedallion <liamlaverty6@gmail.com>
2020-11-01 12:51:27 +0000

    Camera Jump

---
+++ Assets/Scripts/Camera/CameraJump.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+
+public class CameraJump : MonoBehaviour
+{
+
+    
+    public Camera HellCamera, HeavanCamera;
+    public KeyCode QKey;
+    public bool camSwitch = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(QKey))
+        {
+            camSwitch = !camSwitch;
+            HellCamera.gameObject.SetActive(camSwitch);
+            HeavanCamera.gameObject.SetActive(!camSwitch);
+          
+            
+        }
+    }
+}
