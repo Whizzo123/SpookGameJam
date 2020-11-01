55afd7cfefac312234f76ce5c96d1d68d085b3a9
SilverMedallion <liamlaverty6@gmail.com>
2020-11-01 14:09:00 +0000

    Camera Swithc

---
+++ Assets/Scripts/Camera/CameraSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+
+public class CameraSwitch : MonoBehaviour
+{
+
+
+    public Camera HellCamera, HeavanCamera;
+    public KeyCode SpaceKey;
+    public bool camSwitch = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(SpaceKey))
+        {
+            camSwitch = !camSwitch;
+            HellCamera.gameObject.SetActive(camSwitch);
+            HeavanCamera.gameObject.SetActive(!camSwitch);
+
+
+        }
+    }
+}
